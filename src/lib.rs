924ea7123966a7ead785a760b5237cf116a9bbd3
Jacob Sapoznikow <94275204+RedstoneWizard08@users.noreply.github.com>
2022-06-15 19:52:50 -0700

    Fix HTTP requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 pub mod maven;
+pub mod util;
+
 pub use maven::*;
 
 pub async fn get_forge_versions() -> MavenVersionResult {
-    let client = reqwest::Client::new();
-    let res = client
-        .get("https://maven.minecraftforge.net/net/minecraftforge/forge/maven-metadata.xml")
-        .send()
-        .await;
-
-    let resp = res.unwrap().text().await.unwrap();
+    let resp = util::get(
+        "https://maven.minecraftforge.net/net/minecraftforge/forge/maven-metadata.xml".to_string(),
+    );
     let raw = xmltojson::to_json(&resp).expect("Something went wrong.");
     let serialized = serde_json::to_string(&raw).unwrap();
     let des: RawMavenVersionResult = serde_json::from_str(&serialized).unwrap();
